8557c16da691cb66491fa302cf7fa590cf981d0a
Jesus Ali Rios <47091767+jr1os@users.noreply.github.com>
2021-12-20 19:16:05 -0300

    Update prime_numbers.py
    Correction bug

--- prime_numbers.py
+++ prime_numbers.py
@@ -12,4 +12,9 @@ def next_prime(value, factor=1, **kwargs):
     value = factor * value
     first_value_val = value
 
-    while not check_prime(value)
+    while not check_prime(value):
+        value += 1 if not ("desc" in kwargs.keys() and kwargs["desc"] is True) else -1
+
+    if value == first_value_val:
+        return next_prime(value + 1, **kwargs)
+    return value
